252a6883d4424df28bbb084842be88dca900263e
Pattarapon Makhirun <pattarapon131@gmail.com>
2022-02-06 21:07:21 +0700

    new repo for group's project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  backdropFilter: {
+    'none': 'none',
+    'blur': 'blur(20px)',
+  },
+  plugins: [],
+}
