0326db10a3acf7ffef1410445371a563ef90ce14
Pattarapon Makhirun <pattarapon131@gmail.com>
2022-02-06 21:18:00 +0700

    update url

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
  
 <template>
     <div class="flex items-center justify-center h-screen background from-indigo-500 to-indigo-800">
-        <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs bg-opacity-80
-        bg-clip-padding">
+        <div
+            class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs bg-opacity-80 bg-clip-padding"
+        >
             <img
                 class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto"
                 src="https://mailkmuttacth.sharepoint.com/sites/INT2032-2564Client-sideWebProgrammingII/_api/siteiconmanager/getsitelogo?type='1'&hash=637779203500820903"
@@ -13,27 +14,19 @@
             <h1 class="text-lg text-gray-700">INT 203 | Client-side Web Programming</h1>
             <h3 class="text-sm text-gray-500">Section 2 Group 10</h3>
             <p class="text-xl text-gray-600 mt-4">Group's project is in process...</p>
-            <button
-                class="bg-indigo-600 px-8 py-2 mt-8 rounded-3xl text-gray-100 font-semibold tracking-wide"
-            >GitHub</button>
+            <a href="https://github.com/Joe-sit/INT203-Group-Project">
+                <button
+                    class="bg-indigo-600 px-8 py-2 mt-8 rounded-3xl text-gray-100 font-semibold tracking-wide"
+                >GitHub</button>
+            </a>
         </div>
     </div>
 </template>
  
 <style>
 .background {
-    background: #0f0c29; 
-    background: -webkit-linear-gradient(
-        to right,
-        #24243e,
-        #302b63,
-        #0f0c29
-    ); 
-    background: linear-gradient(
-        to right,
-        #24243e,
-        #302b63,
-        #0f0c29
-    ); 
+    background: #0f0c29;
+    background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);
+    background: linear-gradient(to right, #24243e, #302b63, #0f0c29);
 }
 </style>
